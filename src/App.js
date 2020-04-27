7fbc909fcfe3d81efa04ba32899263b6907324be
huoqingliu <2507575801@qq.com>
2020-04-27 17:39:38 +0800

    2020-4-27: 1、添加antd， 2、优化antd，配置按需引入和自定义主题样式

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Button } from 'antd';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>123</h1>
+         <Button type="primary">Button</Button>
       </div>
     )
   }
