c80cb3dc6bf91e0db5c9c668767bed7fc1975c27
huoqingliu <2507575801@qq.com>
2020-04-29 09:21:31 +0800

    创建redux分支，清空无关代码

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
-import {Switch,Route,Redirect} from 'react-router-dom'
-import Login from "./page/login/login";
-import Admin from "./page/Admin/admin";
 
 export default class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route path='/Admin' component={Admin} />
-        <Route path='/login' component={Login} />
-        <Redirect to='login'/>
-      </Switch>
+      <div>
+        
+      </div>
     )
   }
 }
--- src/api/index.js
+++
@@ -1,3 +0,0 @@
-import ajax from "./ajax";
-
-export const reqLogin = (loginObj)=> ajax.post('http://localhost:3000/login', loginObj)
